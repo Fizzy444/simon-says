4ee3f55803ba97e7f3e3f17e3e9483e802b35afa
Fizzy444 <mr.mithun2521@gmail.com>
2025-03-27 22:09:41 +0530

    game.js

--- game.js
+++ game.js
@@ -6,116 +6,114 @@ var started = false;
 var hardMode = false; // Default is easy mode
 
 $(document).ready(function () {
-	// Handle difficulty selection
-	$("#easy").click(function () {
-		hardMode = false;
-		startGame();
-	});
+    // Handle difficulty selection
+    $("#easy").click(function () {
+        hardMode = false;
+        startGame();
+    });
 
-	$("#hard").click(function () {
-		hardMode = true;
-		startGame();
-	});
+    $("#hard").click(function () {
+        hardMode = true;
+        startGame();
+    });
 });
 
 // Function to start the game
 function startGame() {
-	$("#start-screen").hide(); // Hide difficulty selection
-	$("#game-screen").show();  // Show game screen
-	resetGame();
+    $("#start-screen").hide(); // Hide difficulty selection
+    $("#game-screen").show();  // Show game screen
+    resetGame();
 
-	// Start the game immediately after difficulty selection
-	if (!started) {
-		$("#level-title").text("Level " + level);
-		nextSequence();
-		started = true;
-	}
+    // Start the game immediately after difficulty selection
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
 }
 
 // User button click
 $(".btn").on("click", function () {
-	var userChosenColour = $(this).attr("id");
-	userClickedPattern.push(userChosenColour);
-	animatePress(userChosenColour);
-	playSound(userChosenColour);
-	checkAnswer(userClickedPattern.length - 1);
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    animatePress(userChosenColour);
+    playSound(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
 // Next sequence generation
 function nextSequence() {
-	userClickedPattern = [];
-	level++;
-	$("#level-title").text("Level " + level);
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
 
-	var randomNumber = Math.floor(Math.random() * 4);
-	var randomChosenColour = buttonColours[randomNumber];
-	gamePattern.push(randomChosenColour);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 
-	if (!hardMode) {
-		// EASY MODE: Repeat full sequence
-		for (let i = 0; i < gamePattern.length; i++) {
-			setTimeout(() => {
-				animatePress(gamePattern[i]);
-				playSound(gamePattern[i]);
-			}, i * 600);
-		}
-	} else {
-		// HARD MODE: Show only the new color
-		setTimeout(() => {
-			animatePress(randomChosenColour);
-			playSound(randomChosenColour);
-		}, 600);
-	}
+    if (!hardMode) {
+        // EASY MODE: Repeat full sequence
+        for (let i = 0; i < gamePattern.length; i++) {
+            setTimeout(() => {
+                animatePress(gamePattern[i]);
+                playSound(gamePattern[i]);
+            }, i * 1000); // Slower delay to help remember
+        }
+    } else {
+        // HARD MODE: Show only the new color
+        setTimeout(() => {
+            animatePress(randomChosenColour);
+            playSound(randomChosenColour);
+        }, 600);
+    }
 }
 
 // Play sound function
 function playSound(name) {
-	var audio = new Audio("sounds/" + name + ".mp3");
-	audio.play();
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
 // Animate button press
 function animatePress(currentColour) {
-	$("#" + currentColour).addClass("pressed");
-	setTimeout(function () {
-		$("#" + currentColour).removeClass("pressed");
-	}, 100);
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
 }
 
 // Check user answer
 function checkAnswer(currentLevel) {
-	if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-		if (userClickedPattern.length === gamePattern.length) {
-			setTimeout(nextSequence, 1000);
-		}
-	} else {
-		playSound("wrong");
-		$("body").addClass("game-over");
-		setTimeout(() => {
-			$("body").removeClass("game-over");
-		}, 200);
-		$("#level-title").text("Game Over! Press Any Key to Restart");
-		startOver();
-	}
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(nextSequence, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over! Press Any Key to Restart");
+        startOver();
+    }
 }
 
 // Reset the game
 function resetGame() {
-	gamePattern = [];
-	started = false;
-	level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+    level = 0;
+    started = false;
 }
 
 // Start over after game over
 function startOver() {
-	started = false;
-	gamePattern = [];
-	level = 0;
+    started = false;
+    gamePattern = [];
+    level = 0;
 
-	// Listen for key press to restart
-	$(document).one("keydown", function() {
-		$("#level-title").text("Level " + level);
-		nextSequence();
-		started = true;
-	});
+    // Show difficulty selection again
+    $("#start-screen").show();
+    $("#game-screen").hide();
 }
